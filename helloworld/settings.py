b70c032a34849495bdda209b7e88aa368ae0d3a4
Shabeeb M <74951749+itsmeshabeeb@users.noreply.github.com>
2022-08-16 20:44:31 +0530

    added static_root at settings

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -126,3 +126,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+
